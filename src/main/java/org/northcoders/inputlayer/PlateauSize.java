9cb8e8452f6164d16db6d6689656196d920e3ae9
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-08-15 17:26:27 +0100

    feat: add getter and setters

--- src/main/java/org/northcoders/inputlayer/PlateauSize.java
+++ src/main/java/org/northcoders/inputlayer/PlateauSize.java
@@ -4,4 +4,19 @@ public class PlateauSize {
     private int maxX;
     private int maxY;
 
+    public int getMaxX() {
+        return maxX;
+    }
+
+    public void setMaxX(int maxX) {
+        this.maxX = maxX;
+    }
+
+    public int getMaxY() {
+        return maxY;
+    }
+
+    public void setMaxY(int maxY) {
+        this.maxY = maxY;
+    }
 }
