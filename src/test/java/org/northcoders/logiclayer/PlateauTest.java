afd26ee76812695c721e7269358fcdd79735b474
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-08-15 17:28:29 +0100

    test: Create test for Plateau constructor

--- src/test/java/org/northcoders/logiclayer/PlateauTest.java
+++ src/test/java/org/northcoders/logiclayer/PlateauTest.java
@@ -1,4 +1,25 @@
+package org.northcoders.logiclayer;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PlateauTest {
-  
+
+    Plateau plateau;
+
+    @Test
+    void plateauConstructor_validDimension() {
+        plateau = new Plateau(new int[] {3,4});
+        assertEquals(3, plateau.getMaxX());
+        assertEquals(4, plateau.getMaxY());
+    }
+
+
+
+
 }
