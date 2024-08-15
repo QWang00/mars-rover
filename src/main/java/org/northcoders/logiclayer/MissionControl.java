24b8c1cb819a57cfff4b3d28921036d243971ec8
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-08-15 15:24:25 +0100

    feat: Create class MissionControl

--- src/main/java/org/northcoders/logiclayer/MissionControl.java
+++ src/main/java/org/northcoders/logiclayer/MissionControl.java
@@ -1,2 +1,22 @@
-package org.northcoders.logiclayer;public class MissionControl {
+package org.northcoders.logiclayer;
+
+import org.northcoders.inputlayer.Instruction;
+import org.northcoders.inputlayer.RoverPosition;
+
+
+public class MissionControl {
+    private Instruction instruction;
+
+    public RoverPosition getRoverPosition(String roverID){
+        return new RoverPosition();
+    }
+
+    public void landRover (String roverID, RoverPosition position){
+
+    }
+
+    public void moveRover(String roverID, Instruction instruction){
+
+    }
+
 }
