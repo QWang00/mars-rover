c82e20db68266b90d440e0e187408b38df420880
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-08-15 17:29:19 +0100

    fix: Change Plateau to a class

--- src/main/java/org/northcoders/logiclayer/Plateau.java
+++ src/main/java/org/northcoders/logiclayer/Plateau.java
@@ -2,5 +2,31 @@ package org.northcoders.logiclayer;
 
 import org.northcoders.inputlayer.PlateauSize;
 
-public record Plateau(PlateauSize maxX, PlateauSize maxY) {
+public class Plateau{
+    private int maxX;
+    private int maxY;
+
+    public Plateau(int[] dimensions) {
+            this.maxX = dimensions[0];
+            this.maxY = dimensions[1];
+    }
+
+    public Plateau() {
+    }
+
+    public int getMaxX() {
+        return maxX;
+    }
+
+    public void setMaxX(int maxX) {
+        this.maxX = maxX;
+    }
+
+    public int getMaxY() {
+        return maxY;
+    }
+
+    public void setMaxY(int maxY) {
+        this.maxY = maxY;
+    }
 }
