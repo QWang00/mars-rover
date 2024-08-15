1887fd4db9357ca338b3ad094ea3b311a8fd4014
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-08-15 15:23:49 +0100

    feat: create class Rover with a builder

--- src/main/java/org/northcoders/logiclayer/Rover.java
+++ src/main/java/org/northcoders/logiclayer/Rover.java
@@ -2,13 +2,43 @@ package org.northcoders.logiclayer;
 
 import org.northcoders.inputlayer.RoverPosition;
 
-public class Rovers {
+public class Rover {
     private final String roverID;
     private final String name;
     private final RoverPosition position;
 
-    private Rovers(RoverBuilder builder){
-        this.name = builder.
+    private Rover(RoverBuilder builder){
+        this.name = builder.name;
+        this.position = builder.position;
+        this.roverID = builder.roverID;
     }
+
+    public class RoverBuilder {
+        private String roverID;
+        private String name;
+        private RoverPosition position;
+
+        public RoverBuilder robotID(String roverID) {
+            this.roverID = roverID;
+            return this;
+        }
+
+        public RoverBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public RoverBuilder position(RoverPosition position) {
+            this.position = position;
+            return this;
+        }
+
+        public Rover build() {
+            return new Rover(this);
+        }
+
+
+    }
+
 
 }
