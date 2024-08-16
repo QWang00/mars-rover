c89a3142fd2efa962e32275bbc5c953177df216d
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-08-16 09:38:06 +0100

    feat: Add constructor, getters, setters for RoverPosition

--- src/main/java/org/northcoders/inputlayer/RoverPosition.java
+++ src/main/java/org/northcoders/inputlayer/RoverPosition.java
@@ -5,5 +5,33 @@ public class RoverPosition {
     private int y;
     private CompassDirection facing;
 
+    public RoverPosition(int x, int y, CompassDirection facing) {
+        this.x = x;
+        this.y = y;
+        this.facing = facing;
+    }
 
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public CompassDirection getFacing() {
+        return facing;
+    }
+
+    public void setFacing(CompassDirection facing) {
+        this.facing = facing;
+    }
 }
