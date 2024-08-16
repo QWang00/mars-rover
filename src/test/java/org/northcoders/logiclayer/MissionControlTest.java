7007fd2292fa6099c25407adb7613a21c55baf4c
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-08-16 09:56:06 +0100

    test: Initialize test methods for MissionControl

---
+++ src/test/java/org/northcoders/logiclayer/MissionControlTest.java
@@ -0,0 +1,26 @@
+package org.northcoders.logiclayer;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MissionControlTest {
+
+    MissionControl missionControl = new MissionControl();
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getRoverPosition() {
+    }
+
+    @Test
+    void landRover() {
+    }
+
+    @Test
+    void moveRover() {
+    }
+}
