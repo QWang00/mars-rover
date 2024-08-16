41cb9c73785fcb40242a6719ad8241f304f0792f
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-08-16 15:25:51 +0100

    test: Add tests for landRoverToPlateau

--- src/test/java/org/northcoders/logiclayer/MissionControlTest.java
+++ src/test/java/org/northcoders/logiclayer/MissionControlTest.java
@@ -2,40 +2,93 @@ package org.northcoders.logiclayer;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.northcoders.inputlayer.CompassDirection;
+import org.northcoders.inputlayer.RoverPosition;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MissionControlTest {
 
     MissionControl missionControl;
-    Map<String, Rover> rovers;
+
+
     @BeforeEach
     void setUp() {
-        missionControl = new MissionControl(null, null);
-        rovers = new HashMap<>();
+        missionControl = new MissionControl();
+
     }
 
     @Test
     void addRover_EmptyRover() {
-        assertThrows(IllegalArgumentException.class,()-> missionControl.addRover("", "", rovers));
-        assertThrows(IllegalArgumentException.class,()-> missionControl.addRover("R1", "", rovers));
-        assertThrows(IllegalArgumentException.class,()-> missionControl.addRover("", "Rover1", rovers));
+        assertThrows(IllegalArgumentException.class,()-> missionControl.addRover("", ""));
+        assertThrows(IllegalArgumentException.class,()-> missionControl.addRover("R1", ""));
+        assertThrows(IllegalArgumentException.class,()-> missionControl.addRover("", "Rover1"));
     }
     @Test
     void addRover_NullRover() {
-        assertThrows(IllegalArgumentException.class,()-> missionControl.addRover(null, null, rovers));
-        assertThrows(IllegalArgumentException.class,()-> missionControl.addRover("R1", null, rovers));
-        assertThrows(IllegalArgumentException.class,()-> missionControl.addRover(null, "Rover1", rovers));
+        assertThrows(IllegalArgumentException.class,()-> missionControl.addRover(null, null));
+        assertThrows(IllegalArgumentException.class,()-> missionControl.addRover("R1", null));
+        assertThrows(IllegalArgumentException.class,()-> missionControl.addRover(null, "Rover1"));
 
     }
     @Test
     void addRover_ValidRoverAddedToMap() {
-        missionControl.addRover("R1", "Rover1", rovers);
-        assertEquals(1, rovers.size());
-        assertEquals("Rover1", rovers.get("R1").getName());
+        missionControl.addRover("R1", "Rover1");
+        assertEquals(1, missionControl.getRovers().size());
+        assertEquals("Rover1", missionControl.getRovers().get("R1").getName());
+
+    }
+
+
+    @Test
+    void landRoverToPlateau_EmptyFields() {
+        RoverPosition position = new RoverPosition(10,3, CompassDirection.S);
+        Plateau plateau = new Plateau(new int[]{15, 7}, "P1");
+        missionControl.addRover("R1", "Rover1");
+        assertThrows(IllegalArgumentException.class,()->missionControl.landRoverToPlateau("", position, plateau));
+
+    }
+
+    @Test
+    void landRoverToPlateau_NullFields() {
+        RoverPosition position = new RoverPosition(10,3, CompassDirection.S);
+        Plateau plateau = new Plateau(new int[]{15, 7}, "P1");
+        missionControl.addRover("R1", "Rover1");
+        assertThrows(IllegalArgumentException.class,()->missionControl.landRoverToPlateau(null,null, null));
+        assertThrows(IllegalArgumentException.class,()->missionControl.landRoverToPlateau(null, position, plateau));
+        assertThrows(IllegalArgumentException.class,()->missionControl.landRoverToPlateau("R1", null, plateau));
+        assertThrows(IllegalArgumentException.class,()->missionControl.landRoverToPlateau("R1", position, null));
+    }
+
+    @Test
+    void landRoverToPlateau_RoverNotInMap() {
+        RoverPosition position = new RoverPosition(10,3, CompassDirection.S);
+        Plateau plateau = new Plateau(new int[]{15, 7}, "P1");
+
+        Rover rover = new Rover.RoverBuilder()
+                .robotID("R1")
+                .name("Rover1")
+                .build();
+        assertThrows(IllegalArgumentException.class,()->missionControl.landRoverToPlateau("R1", position, plateau));
+
+    }
+
+    @Test
+    void landRoverToPlateau_Valid(){
+        RoverPosition position = new RoverPosition(10, 3, CompassDirection.S);
+        Plateau plateau = new Plateau(new int[]{15, 7}, "P1");
+
+        missionControl.addRover("R1", "Rover1");
+        assertEquals(1, missionControl.getRovers().size());
+        assertTrue(missionControl.getRovers().containsKey("R1"));
+        missionControl.landRoverToPlateau("R1", position, plateau);
+
+        Rover rover = missionControl.getRovers().get("R1");
+        assertNotNull(rover);
+        assertEquals(position, rover.getPosition());
+        assertEquals(plateau, rover.getPlateau());
 
     }
 
@@ -43,11 +96,6 @@ class MissionControlTest {
     @Test
     void getRoverPosition() {
     }
-
-    @Test
-    void landRover() {
-    }
-
 
     @Test
     void moveRover() {
