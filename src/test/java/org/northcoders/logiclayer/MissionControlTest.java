df330e26676f27bcc9daa7ef78d237e90158cf57
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-08-16 11:25:56 +0100

    test: add & pass tests for addRover method

--- src/test/java/org/northcoders/logiclayer/MissionControlTest.java
+++ src/test/java/org/northcoders/logiclayer/MissionControlTest.java
@@ -3,14 +3,42 @@ package org.northcoders.logiclayer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MissionControlTest {
 
-    MissionControl missionControl = new MissionControl();
+    MissionControl missionControl;
+    Map<String, Rover> rovers;
     @BeforeEach
     void setUp() {
+        missionControl = new MissionControl(null, null);
+        rovers = new HashMap<>();
     }
+
+    @Test
+    void addRover_EmptyRover() {
+        assertThrows(IllegalArgumentException.class,()-> missionControl.addRover("", "", rovers));
+        assertThrows(IllegalArgumentException.class,()-> missionControl.addRover("R1", "", rovers));
+        assertThrows(IllegalArgumentException.class,()-> missionControl.addRover("", "Rover1", rovers));
+    }
+    @Test
+    void addRover_NullRover() {
+        assertThrows(IllegalArgumentException.class,()-> missionControl.addRover(null, null, rovers));
+        assertThrows(IllegalArgumentException.class,()-> missionControl.addRover("R1", null, rovers));
+        assertThrows(IllegalArgumentException.class,()-> missionControl.addRover(null, "Rover1", rovers));
+
+    }
+    @Test
+    void addRover_ValidRoverAddedToMap() {
+        missionControl.addRover("R1", "Rover1", rovers);
+        assertEquals(1, rovers.size());
+        assertEquals("Rover1", rovers.get("R1").getName());
+
+    }
+
 
     @Test
     void getRoverPosition() {
@@ -20,7 +48,12 @@ class MissionControlTest {
     void landRover() {
     }
 
+
     @Test
     void moveRover() {
+    }
+
+    @Test
+    void moveRoversSequentially() {
     }
 }
