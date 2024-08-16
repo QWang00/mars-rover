56b9d0b861cca396224a5a6b5141b12bdd8176e8
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-08-16 14:06:31 +0100

    test: Create & pass tests to verify position exceeds plateau dimention

--- src/test/java/org/northcoders/logiclayer/RoverTest.java
+++ src/test/java/org/northcoders/logiclayer/RoverTest.java
@@ -20,9 +20,11 @@ class RoverTest {
 
     @Test
     public void testRoverBuilder_SetsMutableFieldsCorrectly(){
+        Plateau plateau = new Plateau(new int[]{5,6}, "p1");
         Rover rover = new Rover.RoverBuilder()
                 .name("Wulong")
                 .position(new RoverPosition(2,4, CompassDirection.N))
+                .plateau(plateau)
                 .build();
         rover.setName("Wu Long");
         rover.setPosition(new RoverPosition(3,5,CompassDirection.S));
@@ -30,5 +32,35 @@ class RoverTest {
         assertEquals(CompassDirection.S, rover.getPosition().getFacing());
         assertEquals(3, rover.getPosition().getX());
         assertEquals(5, rover.getPosition().getY());
+    }
+
+    @Test
+    public void getPosition_PositionExceedsPlateauDimension(){
+        Plateau plateau = new Plateau(new int[]{4,5}, "P1");
+        Rover rover = new Rover.RoverBuilder()
+                .name("Wulong")
+                .plateau(plateau)
+                .build();
+        assertThrows(InvalidPositionException.class,()->rover.setPosition(new RoverPosition(5,7,CompassDirection.N)));
+        assertThrows(InvalidPositionException.class,()->rover.setPosition(new RoverPosition(5,1,CompassDirection.N)));
+        assertThrows(InvalidPositionException.class,()->rover.setPosition(new RoverPosition(1,7,CompassDirection.N)));
+    }
+
+    @Test
+    public void getPosition_PositionWithinPlateauDimension(){
+        Plateau plateau = new Plateau(new int[]{4,5}, "P1");
+        Rover rover = new Rover.RoverBuilder()
+                .name("Wulong")
+                .plateau(plateau)
+                .build();
+
+        rover.setPosition(new RoverPosition(4,5,CompassDirection.N));
+        assertEquals(4, rover.getPosition().getX());
+        assertEquals(5, rover.getPosition().getY());
+
+        rover.setPosition(new RoverPosition(1,1,CompassDirection.N));
+        assertEquals(1, rover.getPosition().getX());
+        assertEquals(1, rover.getPosition().getY());
+
     }
 }
