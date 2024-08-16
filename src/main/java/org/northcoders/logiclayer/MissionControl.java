8b687ace640ec34043b46af88d3ac7726c1529c4
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-08-16 10:32:02 +0100

    feat: Reorganize the structure of MissionControl

--- src/main/java/org/northcoders/logiclayer/MissionControl.java
+++ src/main/java/org/northcoders/logiclayer/MissionControl.java
@@ -3,23 +3,48 @@ package org.northcoders.logiclayer;
 import org.northcoders.inputlayer.Instruction;
 import org.northcoders.inputlayer.RoverPosition;
 
+import java.util.Map;
+
 
 public class MissionControl {
-    //private Instruction instruction;
 
-    public RoverPosition getRoverPosition(Rover rover){
-        return rover.getPosition();
+    private Plateau plateau;
+    private Map<String, Rover> rovers;
+
+    public MissionControl(Plateau plateau, Map<String, Rover> rovers) {
+        this.plateau = plateau;
+        this.rovers = rovers;
     }
 
-    public void landRover (String roverID, RoverPosition position){
+    public void addRover (String id, String name){
 
     }
 
-    public void moveRover(String roverID, Instruction instruction){
+    public RoverPosition getRoverPosition(String roverID) {
+        return null;
+    }
+
+    public void landRover(String roverID, RoverPosition startingPosition) {
 
     }
 
-//    public MissionControl(Instruction instruction) {
-//        this.instruction = instruction;
-//    }
+    public void moveRover(String roverID, Instruction instruction) {
+
+    }
+
+    public Plateau getPlateau() {
+        return plateau;
+    }
+
+    public void setPlateau(Plateau plateau) {
+        this.plateau = plateau;
+    }
+
+    public Map<String, Rover> getRovers() {
+        return rovers;
+    }
+
+    public void setRovers(Map<String, Rover> rovers) {
+        this.rovers = rovers;
+    }
 }
