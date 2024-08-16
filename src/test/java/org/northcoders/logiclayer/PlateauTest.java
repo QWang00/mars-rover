2939a65ca5cb8b2ed48214122e3bbfae150fec50
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-08-16 14:10:48 +0100

    fix: update the constructor in test

--- src/test/java/org/northcoders/logiclayer/PlateauTest.java
+++ src/test/java/org/northcoders/logiclayer/PlateauTest.java
@@ -14,9 +14,10 @@ class PlateauTest {
 
     @Test
     void plateauConstructor_validDimension() {
-        plateau = new Plateau(new int[] {3,4});
+        plateau = new Plateau(new int[] {3,4}, "P1");
         assertEquals(3, plateau.getMaxX());
         assertEquals(4, plateau.getMaxY());
+        assertEquals("P1", plateau.getName());
     }
 
 
