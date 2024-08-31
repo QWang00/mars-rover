4a6e03a08d762e7b2d84720b5414197c8b0cb7bf
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-08-31 21:52:50 +0100

    feat: create simulation method signatures in Simulation class

--- src/main/java/org/northcoders/ui/Simulation.java
+++ src/main/java/org/northcoders/ui/Simulation.java
@@ -1,4 +1,27 @@
 package org.northcoders.ui;
 
+import org.northcoders.logic.Rover;
+
 public class Simulation {
+    public void createPlateau() {
+    }
+
+    public Rover createRover() {
+        return null;
+    }
+
+    public void landRoverToPlateau(Rover rover1) {
+    }
+
+    public void moveRoverByInstructions(Rover rover1) {
+    }
+
+    public void simulateEdgeCollision(Rover rover1) {
+    }
+
+    public void simulateRoverCollision(Rover rover1, Rover rover2) {
+    }
+
+    public void endSimulation() {
+    }
 }
