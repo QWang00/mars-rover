6782b6908cddaf43757b9466b79aef5702b4c7d3
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-08-31 23:09:09 +0100

    feat: implement createPlateau in Simulation class

--- src/main/java/org/northcoders/ui/Simulation.java
+++ src/main/java/org/northcoders/ui/Simulation.java
@@ -1,9 +1,47 @@
 package org.northcoders.ui;
 
+import org.northcoders.logic.MissionControl;
 import org.northcoders.logic.Rover;
+import org.northcoders.model.Plateau;
+import org.northcoders.util.InputParser;
+
+import java.util.Scanner;
 
 public class Simulation {
+    private Plateau plateau;
+    private MissionControl missionControl;
+    private InputParser inputParser;
+    private Scanner scanner;
+
+    public Simulation() {
+        this.missionControl = new MissionControl();
+        this.inputParser = new InputParser();
+        this.scanner = new Scanner(System.in);
+    }
+
     public void createPlateau() {
+        while (true) {
+            try {
+                System.out.println("Enter the width of the plateau:");
+                String width = scanner.nextLine();
+                System.out.println("Enter the height of the plateau:");
+                String height = scanner.nextLine();
+                System.out.println("Enter the name of the plateau:");
+                String name = scanner.nextLine();
+
+                int[] size = inputParser.parsePlateauSize(width, height);
+                plateau = new Plateau(size, name);
+                System.out.println("Plateau created with width: "
+                        + size[0] + " , height "
+                        + size[1] + " with name of "
+                        + name);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("Invalid input for plateau size. Please enter valid numbers greater than zero.");
+            } catch (Exception e) {
+                System.out.println("Unexpected error. Please try again.");
+            }
+        }
     }
 
     public Rover createRover() {
